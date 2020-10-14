94864a372d92bbc60307b1c9bb6306eca23846ac
Ben Gillbanks <binarymoon@gmail.com>
2020-10-14 21:02:15 +0100

    simplify js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,11 +12,7 @@
 
 {% include_relative mwp-theme-list.js %}
 
-{% include_relative mwp-check-mailchimp.js %}
-
 {% include_relative mwp-countdown.js %}
-
-{% include_relative jquery.slider.js %}
 
 ;( function( window, document, $ ) {
 
@@ -34,16 +30,7 @@
 			MWP_ThemeList.init();
 			MWP_ResponsiveIframe.init();
 			MWP_Countdown.init( '.countdown-clock' );
-			MWP_CheckMailchimp.init();
 			MWP_Contact.init();
-
-			$( '.testimonials' ).elementalSlides(
-				{
-					group_selector: '.testimonial',
-					autoplay: 1,
-					nav_arrows: true,
-				}
-			);
 
 		}
 	);
---
+++ assets/scripts/mwp-testimonials.js
@@ -0,0 +1,11 @@
+var MWP_testimonials = function() {
+
+	$( '.testimonials' ).elementalSlides(
+		{
+			group_selector: '.testimonial',
+			autoplay: 1,
+			nav_arrows: true,
+		}
+	);
+
+};
